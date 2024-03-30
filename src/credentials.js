f8662b61aa99df3d36c570c5ecbdbd1d4e967d4d
MarcpTr <marcphtr@gmail.com>
2024-03-30 02:41:35 +0000

    update

---
+++ src/credentials.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDz6NqbpxaHOS16vQszEo0vpA6y9JRfxEE",
+    authDomain: "react-collect.firebaseapp.com",
+    projectId: "react-collect",
+    storageBucket: "react-collect.appspot.com",
+    messagingSenderId: "114230736322",
+    appId: "1:114230736322:web:02c4e10ac855d630da125b",
+};
+
+export default firebaseConfig;
